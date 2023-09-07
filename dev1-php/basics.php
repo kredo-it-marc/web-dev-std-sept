b8b69fc1561ea649dab6e83dd641f7d68df65a48
kredo-it-marc <83500146+kredo-it-marc@users.noreply.github.com>
2023-09-07 21:25:55 +0800

    Day 4

---
+++ dev1-php/basics.php
@@ -0,0 +1,18 @@
+<?php
+    echo "Hello";
+    echo "<h1>Headline 1</h1>";
+    echo "Tim","John"; //display two texts with one echo
+    echo "first"."second"."third";
+
+    print "Hello";
+    print "<h1>Headline 1</h1>";
+   // print "Tim","John"; //print cannot display 2 or more words
+
+    //Variables => store data
+    echo "<hr>";
+    $first_name = "John";
+    $last_name = "Smith";
+    $age = 21;
+    
+    echo "<p>My name is $first_name $last_name. I am $age years old.</p>";
+?>
